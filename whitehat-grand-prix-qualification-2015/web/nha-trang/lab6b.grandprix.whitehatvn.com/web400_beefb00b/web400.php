dcd835365de1bcf7766efb0f277ba7b3a1bb44c1
fariskhi <fariskhi.vidyan@ui.ac.id>
2015-10-31 17:24:35 +0700

    :floppy_disk: Add web resources for whitehat-grand-prix-qualification-2015

---
+++ whitehat-grand-prix-qualification-2015/web/nha-trang/lab6b.grandprix.whitehatvn.com/web400_beefb00b/web400.php
@@ -0,0 +1,15 @@
+<html>
+<title>
+An0nym0us W3b Pr0xy - Trial Version
+</title>
+
+		
+
+	 <form action="#" method=GET>
+	  Enter your trial product key:<br>
+	  <input type="text" name="token" value="">
+	  <br>
+	  <input type="submit" value="Submit">
+	</form> 
+
+	
